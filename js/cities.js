eeefcc53c9dec7250bd95a96f1208db786801247
I.Smal <serbov@pochta.ru>
2017-04-14 22:33:56 +0300

    script for cities array

---
+++ js/cities.js
@@ -0,0 +1,18 @@
+var cities = [
+	{
+		"coordinates": {lat: 60.7185054, lng: 40.37210379999999},
+		"title": "Москва",
+		"distance": "7.8",
+		"likes": "450",
+		"linkToPhoto": "#moscow",
+		"linkToPage": "#moscow"
+	},
+	{
+		"coordinates": {lat: 55.708327, lng: 52.392155},
+		"title": "Набережные Челны",
+		"distance": "15",
+		"likes": "150",
+		"linkToPhoto": "#chelny",
+		"linkToPage": "#chelny"
+	}
+];
--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
 		//cache dom
 		var mapContainer = document.querySelector('#map');
-		var boxText = document.querySelector('#infobox-template').content.querySelector('.infobox');
+		var boxText = document.querySelector('#infobox-template').content.querySelector('.js-infobox');
 
 		initMap();
 
@@ -32,6 +32,7 @@ document.addEventListener('DOMContentLoaded', function(){
 			//cities
 			var myLatLng = {lat: 55.708327, lng: 52.392155};
 
+			//map
 			map = new google.maps.Map(mapContainer, {
 				center: myLatLng,
 				zoom: 6,
@@ -43,32 +44,27 @@ document.addEventListener('DOMContentLoaded', function(){
 				}]
 			});
 
-			var geocoder = new google.maps.Geocoder();
-
-			geocodeAddress(geocoder, map);
-		}
-
-		//geocoder
-		function geocodeAddress(geocoder, resultsMap) {
-			var address = 'Набережные челны';
+			//marker Image
 			var markerImage = new google.maps.MarkerImage('../img/marker.png',
 				new google.maps.Size(15, 15),
 				new google.maps.Point(0, 0),
 				new google.maps.Point(7, 7)
 			);
 
-			geocoder.geocode({'address': address}, function(results, status) {
-				if (status === 'OK') {
-					resultsMap.setCenter(results[0].geometry.location);
-					var marker = new google.maps.Marker({
-						map: resultsMap,
-						position: results[0].geometry.location,
-						icon: markerImage
-					});
+			//markers
+			var markers = [];
+			var ib = [];
+			var ibOptions = [];
 
-					var smalled;
+			cities.forEach(function(city, i) {
+				markers.push(new google.maps.Marker({
+					map: map,
+					position: city.coordinates,
+					icon: markerImage
+				}));
 
-					var myOptions = {
+				ibOptions.push(
+					{
 						content: boxText.cloneNode(true)
 						,disableAutoPan: false
 						,maxWidth: 0
@@ -79,39 +75,160 @@ document.addEventListener('DOMContentLoaded', function(){
 						,isHidden: false
 						,pane: "floatPane"
 						,enableEventPropagation: false
-					};
-
-					google.maps.event.addListener(marker, "click", function (e) {
-						ib.open(map, this);
-					});
-
-					var ib = new InfoBox(myOptions);
-
-					ib.open(map, marker);
-
-					window.addEventListener('resize', _.throttle(resizer, 100));
-
-					//resize
-					function resizer() {
-
-						var small = function() { return window.innerWidth <= 1023; };
-
-						if(small() && smalled) return;
-						if(!small() && !smalled && smalled != 'undefined') return;
-
-						if(small()) {
-							ib.setOptions({pixelOffset: new google.maps.Size(-136, -337)});
-							smalled = true;
-						} else {
-							ib.setOptions({pixelOffset: new google.maps.Size(0, -377)});
-							smalled = false;
-						}
 					}
+				);
 
-				} else {
-					console.log('Geocode was not successful for the following reason: ' + status);
-				}
+				ibOptions[i].content.querySelector('.js-city').innerHTML = city.title;
+				ibOptions[i].content.querySelector('.js-distance').innerHTML = city.distance;
+				ibOptions[i].content.querySelector('.js-likes').innerHTML = city.likes;
+				ibOptions[i].content.querySelector('.js-photos').href = city.linkToPhoto;
+				ibOptions[i].content.querySelector('.js-link').href = city.linkToPage;
+
+				ib.push(new InfoBox(ibOptions[i]));
+
+				google.maps.event.addListener(markers[i], "click", function (e) {
+					ib.forEach(function(_ib) {
+						_ib.close();
+					});
+					ib[i].open(map, this);
+				});
+
+				google.maps.event.addListener(map, "click", function (e) {
+					ib[i].close();
+				});
 			});
+
+			// var address = {lat: 60.7185054, lng: 40.37210379999999};
+			//
+			// map.setCenter(address);
+			// var marker = new google.maps.Marker({
+			// 	map: map,
+			// 	position: address,
+			// 	icon: markerImage
+			// });
+
+			// var myOptions = {
+			// 	content: boxText.cloneNode(true)
+			// 	,disableAutoPan: false
+			// 	,maxWidth: 0
+			// 	,pixelOffset: new google.maps.Size(window.innerWidth <= 1023 ? -136 : 0, window.innerWidth <= 1023 ? -337 : -377)
+			// 	,zIndex: null
+			// 	,closeBoxURL: ""
+			// 	,infoBoxClearance: new google.maps.Size(1, 1)
+			// 	,isHidden: false
+			// 	,pane: "floatPane"
+			// 	,enableEventPropagation: false
+			// };
+			//
+			// google.maps.event.addListener(marker, "click", function (e) {
+			// 	ib.open(map, this);
+			// });
+			//
+			// google.maps.event.addListener(map, "click", function (e) {
+			// 	ib.close();
+			// });
+			//
+			// var ib = new InfoBox(myOptions);
+
+			// ib.open(map, marker);
+
+			window.addEventListener('resize', _.throttle(resizer, 100));
+
+			//resize
+			var smalled;
+
+			function resizer() {
+
+				var small = function() { return window.innerWidth <= 1023; };
+
+				if(small() && smalled) return;
+				if(!small() && !smalled && smalled != 'undefined') return;
+
+				if(small()) {
+					ib.forEach(function(_ib) {
+						_ib.setOptions({pixelOffset: new google.maps.Size(-136, -337)});
+					});
+					smalled = true;
+				} else {
+					ib.forEach(function(_ib) {
+						_ib.setOptions({pixelOffset: new google.maps.Size(0, -377)});
+					});
+					smalled = false;
+				}
+			}
+
+
+
+
+
+			// var geocoder = new google.maps.Geocoder();
+
+			// geocodeAddress(geocoder, map);
+		}
+
+		//geocoder
+		function geocodeAddress(geocoder, resultsMap) {
+
+
+			// geocoder.geocode({'address': address}, function(results, status) {
+			// 	if (status === 'OK') {
+			// 		resultsMap.setCenter(results[0].geometry.location);
+			// 		var marker = new google.maps.Marker({
+			// 			map: resultsMap,
+			// 			position: results[0].geometry.location,
+			// 			icon: markerImage
+			// 		});
+			//
+			// 		var smalled;
+			//
+			// 		var myOptions = {
+			// 			content: boxText.cloneNode(true)
+			// 			,disableAutoPan: false
+			// 			,maxWidth: 0
+			// 			,pixelOffset: new google.maps.Size(window.innerWidth <= 1023 ? -136 : 0, window.innerWidth <= 1023 ? -337 : -377)
+			// 			,zIndex: null
+			// 			,closeBoxURL: ""
+			// 			,infoBoxClearance: new google.maps.Size(1, 1)
+			// 			,isHidden: false
+			// 			,pane: "floatPane"
+			// 			,enableEventPropagation: false
+			// 		};
+			//
+			// 		google.maps.event.addListener(marker, "click", function (e) {
+			// 			ib.open(map, this);
+			// 		});
+			//
+			// 		google.maps.event.addListener(map, "click", function (e) {
+			// 			ib.close();
+			// 		});
+			//
+			// 		var ib = new InfoBox(myOptions);
+			//
+			// 		ib.open(map, marker);
+			//
+			// 		window.addEventListener('resize', _.throttle(resizer, 100));
+			//
+			// 		//resize
+			// 		function resizer() {
+			//
+			// 			var small = function() { return window.innerWidth <= 1023; };
+			//
+			// 			if(small() && smalled) return;
+			// 			if(!small() && !smalled && smalled != 'undefined') return;
+			//
+			// 			if(small()) {
+			// 				ib.setOptions({pixelOffset: new google.maps.Size(-136, -337)});
+			// 				smalled = true;
+			// 			} else {
+			// 				ib.setOptions({pixelOffset: new google.maps.Size(0, -377)});
+			// 				smalled = false;
+			// 			}
+			// 		}
+			//
+			// 	} else {
+			// 		console.log('Geocode was not successful for the following reason: ' + status);
+			// 	}
+			// });
 		}
 	})();
 });
