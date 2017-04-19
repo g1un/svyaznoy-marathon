085fa8048ae397513a1416be8037a521c8ba6605
I.Smal <serbov@pochta.ru>
2017-04-20 00:03:49 +0300

    improvements

--- js/cities.js
+++ js/cities.js
@@ -3,7 +3,7 @@ var cities = [
 		"id": 1,
 		"coordinates": {lat: 60.7185054, lng: 40.37210379999999},
 		"title": "Москва",
-		"distance": "7.8",
+		"distance": "0",
 		"likes": "450",
 		"url": "url1",
 		"linkToPhoto": "1"
@@ -12,7 +12,7 @@ var cities = [
 		"id": 2,
 		"coordinates": {lat: 55.708327, lng: 52.392155},
 		"title": "Набережные Челны",
-		"distance": "15",
+		"distance": "150",
 		"likes": "150",
 		"url": "url2",
 		"linkToPhoto": "2"
--- js/script.js
+++ js/script.js
@@ -37,6 +37,11 @@ document.addEventListener('DOMContentLoaded', function(){
 				center: myLatLng,
 				zoom: 6,
 				disableDefaultUI: true,
+				zoomControl: true,
+				zoomControlOptions: {
+					position: google.maps.ControlPosition.RIGHT_CENTER
+				},
+
 				styles: [{
 					"stylers": [
 						{ "saturation": -100 }
@@ -44,11 +49,92 @@ document.addEventListener('DOMContentLoaded', function(){
 				}]
 			});
 
+			//geolocation button
+			function addYourLocationButton(map, marker)
+			{
+				var controlDiv = document.createElement('div');
+
+				var firstChild = document.createElement('button');
+				firstChild.style.backgroundColor = '#fff';
+				firstChild.style.border = 'none';
+				firstChild.style.outline = 'none';
+				firstChild.style.width = '28px';
+				firstChild.style.height = '28px';
+				firstChild.style.borderRadius = '2px';
+				firstChild.style.boxShadow = '0 1px 4px rgba(0,0,0,0.3)';
+				firstChild.style.cursor = 'pointer';
+				firstChild.style.marginRight = '10px';
+				firstChild.style.padding = '0px';
+				firstChild.title = 'Your Location';
+				controlDiv.appendChild(firstChild);
+
+				var secondChild = document.createElement('div');
+				secondChild.style.margin = '5px';
+				secondChild.style.width = '18px';
+				secondChild.style.height = '18px';
+				secondChild.style.backgroundImage = 'url(https://maps.gstatic.com/tactile/mylocation/mylocation-sprite-1x.png)';
+				secondChild.style.backgroundSize = '180px 18px';
+				secondChild.style.backgroundPosition = '0px 0px';
+				secondChild.style.backgroundRepeat = 'no-repeat';
+				secondChild.id = 'you_location_img';
+				firstChild.appendChild(secondChild);
+
+				google.maps.event.addListener(map, 'dragend', function() {
+					document.querySelector('#you_location_img').style.backgroundPosition =  '0px 0px';
+				});
+
+				firstChild.addEventListener('click', function() {
+					// var imgX = '0';
+					// var animationInterval = setInterval(function(){
+					// 	if(imgX == '-18') imgX = '0';
+					// 	else imgX = '-18';
+					// 	document.querySelector('#you_location_img').style.backgroundPosition = imgX+'px 0px';
+					// }, 500);
+					if(navigator.geolocation) {
+						navigator.geolocation.getCurrentPosition(function(position) {
+							var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+							marker.setPosition(latlng);
+							map.setCenter(latlng);
+							// clearInterval(animationInterval);
+							document.querySelector('#you_location_img').style.backgroundPosition = '-144px 0px';
+						});
+					}
+					else{
+						// clearInterval(animationInterval);
+						document.querySelector('#you_location_img').style.backgroundPosition = '0px 0px';
+					}
+				});
+
+				controlDiv.index = 0;
+				map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(controlDiv);
+			}
+
+			var myMarker = new google.maps.Marker({
+				map: map,
+				animation: google.maps.Animation.DROP,
+				// position: faisalabad
+			});
+
+			addYourLocationButton(map, myMarker);
+
+
 			//marker Image & position
 			var markerImage = new google.maps.MarkerImage('../img/marker.png',
 				new google.maps.Size(15, 15),
 				new google.maps.Point(0, 0),
 				new google.maps.Point(7, 7)
+			);
+
+			var markerImageNull = new google.maps.MarkerImage('../img/marker-sm-blue.png',
+				new google.maps.Size(7, 7),
+				new google.maps.Point(0, 0),
+				new google.maps.Point(4, 4)
+			);
+
+			var markerImageBig = new google.maps.MarkerImage('../img/marker-lg.png',
+				new google.maps.Size(30, 30),
+				new google.maps.Point(0, 0),
+				new google.maps.Point(15, 15)
 			);
 
 			//markers and infoBlock
@@ -79,7 +165,7 @@ document.addEventListener('DOMContentLoaded', function(){
 				markers[city.id] = new google.maps.Marker({
 					map: map,
 					position: city.coordinates,
-					icon: markerImage
+					icon: city.distance == 0 ? markerImageNull : city.distance < 50 ? markerImage : markerImageBig
 				});
 
 				//bind 'click' event to marker
